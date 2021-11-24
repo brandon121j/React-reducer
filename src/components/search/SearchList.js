9ee3bc43a5303515b777d746a1f59c1346bf153d
Brandon <brandon121j@gmail.com>
2021-11-24 12:12:49 -0500

    Searchlist function up and running

--- src/components/search/SearchList.js
+++ src/components/search/SearchList.js
@@ -3,9 +3,21 @@ import { SearchContext } from '../../context/movieContext';
 
 function SearchList() {
     const { results } = useContext(SearchContext);
+
+    function showMovieList() {
+        return results.map((item, index) => {
+            return (
+                <li key={index}>
+                    <img src={item.Poster} alt='Movie poster'/>
+                    {item.title}
+                </li>
+            )
+        });
+    }
+
     return (
         <div>
-            
+            {showMovieList()}
         </div>
     )
 }
