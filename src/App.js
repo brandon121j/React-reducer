3947bcbf4c017dac67ca071cf71d89695d64459d
Brandon <brandon121j@gmail.com>
2021-11-24 11:45:53 -0500

    Movie search up and running

--- src/App.js
+++ src/App.js
@@ -3,32 +3,33 @@ import { SearchContext, MovieContext } from './context/movieContext';
 
 
 import MovieData from './components/movieData/MovieData';
-import Search from './components/search/SearchList';
+import Search from './components/search/Search';
 
-require('dotenv').config();
 import './App.css';
 
 
 function App() {
-  const key = process.env.REACT_APP_JWT_SECRET;
-  const api = process.env.REACT_APP_API_KEY;
+  let api = process.env.REACT_APP_API_KEY;
   const [searchValue, setSearchValue] = useState('');
   const [results, setResults] = useState([]);
   const [movieSelected, setMovieSelected] = useState(null);
   const [searching, setSearching] = useState(false);
-
+  
   async function handleSearchChange(inputValue) {
     setSearchValue(inputValue);
 
-    const response = await  fetch(`https://www.omdbapi.com/?apikey=${api}&s=${inputValue}&type=movie`);
+    const response = await fetch(`https://www.omdbapi.com/?apikey=${api}&s=${inputValue}&type=movie`);
 
-    const data = response.json();
+    const data = await response.json();
+
+    console.log(data)
 
     setResults(data.Search || []);
     setSearching(true)
   }
 
-  const searchContextValue ={
+  const searchContextValue = {
+    handleSearchChange,
     title: searchValue,
     results,
     searching,
@@ -36,7 +37,7 @@ function App() {
 
 	return (
   <div className="App">
-    <SearchContext.Provider value={{}}>
+    <SearchContext.Provider value={searchContextValue}>
       <Search />
     </SearchContext.Provider>
     <MovieContext.Provider value={{}}>
--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import './index.css';
+import { SearchContext } from '../../context/movieContext';
 
 function Search() {
+    const { title, handleSearchChange, searching } = useContext(SearchContext);
     return (
         <div className='search'>
             <input
             type='search'
             name='movie-search'
+            value={title}
+            onChange={(e) => handleSearchChange(e.target.value)}
             />
         </div>
     )
